3bfaf74e9c13aaecbea183570ca1f66c19180810
willlaptop-PC\willlaptop <wpratt353@gmail.com>
2020-06-01 17:26:37 -0400

    contact add graphql

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StaticQuery } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
